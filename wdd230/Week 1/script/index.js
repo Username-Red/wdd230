c3d44a74258af3e3877c09821149f8504903b2d9
Username-Red <ashkestrel@gmail.com>
2023-04-19 21:50:51 +0200

    Adding JS for the year

--- wdd230/Week 1/script/index.js
+++ wdd230/Week 1/script/index.js
@@ -1,1 +1,7 @@
+
 document.querySelector(".date-last-modified").textContent = new Date(document.lastModified);
+
+const d = new Date;
+let year = d.getFullYear();
+
+document.querySelector(".year").textContent = year;

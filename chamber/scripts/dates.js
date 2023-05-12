9c54a784bf6fcc0f0286edb4be0fbfd454e77594
Username-Red <ashkestrel@gmail.com>
2023-05-12 16:34:17 +0200

    adding things i forgot

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -1,2 +1,17 @@
 document.getElementById("year").innerHTML = new Date().getFullYear()
 document.getElementById("lst-updt").innerHTML = "Last updated: "+document.lastModified
+
+// select the DOM elements to manipulate (we will output to these)
+const datefield = document.querySelector("time");
+// for european/family history format with day first.
+
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+
+
+datefield.textContent = fulldate;

808c316177e3ad32069b8076361ef2ddf46fb900
Username-Red <ashkestrel@gmail.com>
2023-05-20 23:20:31 +0200

    updating and neatening

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -3,7 +3,6 @@ document.getElementById("lst-updt").innerHTML = "Last updated: "+document.lastMo
 
 // select the DOM elements to manipulate (we will output to these)
 const datefield = document.querySelector("time");
-// for european/family history format with day first.
 
 
 // derive the current date using a date object
@@ -15,3 +14,14 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 
 
 datefield.textContent = fulldate;
+
+const current = new Date();
+const day1 = current.getDay();
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const day = dayNames[day1]
+
+if (day == "Monday" || day == "Tuesday") {
+	const datefield = document.querySelector("time");
+	datefield.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m";
+}
+

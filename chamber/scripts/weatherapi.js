620b37a541e000269e11d5c4cde6f8c60b77404f
Username-Red <ashkestrel@gmail.com>
2023-06-22 23:50:41 +0200

    finishing

--- chamber/scripts/weatherapi.js
+++ chamber/scripts/weatherapi.js
@@ -38,8 +38,7 @@ function displaySpeed(weatherData) {
 function windchill(weatherData) {
   var temp = weatherData.main.temp;
   var speed = weatherData.wind.speed;
-  alert(temp);
-  alert(speed);
+  
   if (temp <= 50 && speed >= 3.0) {
     var f = 35.74 + 0.6215 * temp - 35.75 * (speed ** 0.16) + 0.4275 * temp * (speed ** 0.16);
     f = Math.round(f);

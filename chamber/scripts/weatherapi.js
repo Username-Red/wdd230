9c7cd75a2b57d1908aa7cac758dafebcfce7ef92
Username-Red <ashkestrel@gmail.com>
2023-06-22 23:33:47 +0200

    finishing windchill assignment

---
+++ chamber/scripts/weatherapi.js
@@ -0,0 +1,55 @@
+const currentTemp = document.querySelector('.temp');
+const weatherIcon = document.querySelector('.weather-icon');
+// const captionDesc = document.querySelector('figcaption');
+const url = "http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=4f3b6ca284c383b4bbd8fbd38d58c9a0";
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+        displaySpeed(data);
+        windchill(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}&degF</strong>`;
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    //captionDesc.textContent = desc;
+}
+
+function displaySpeed(weatherData) {
+  document.getElementsByClassName("speed")[0].innerHTML = `<strong>${weatherData.wind.speed.toFixed(0)}</strong>`
+} 
+
+function windchill(weatherData) {
+  var temp = weatherData.main.temp;
+  var speed = weatherData.wind.speed;
+  alert(temp);
+  alert(speed);
+  if (temp <= 50 && speed >= 3.0) {
+    var f = 35.74 + 0.6215 * temp - 35.75 * (speed ** 0.16) + 0.4275 * temp * (speed ** 0.16);
+    f = Math.round(f);
+    var chill = f.toString();
+    document.getElementsByClassName("chill")[0].textContent = `${chill} &degF`;
+}
+else {
+    document.getElementsByClassName("chill")[0].textContent = "N/A";
+}
+}
+
+  
+apiFetch();
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,17 +1,17 @@
 
-var temp = document.getElementsByClassName("temp")[0].textContent;
-var speed = document.getElementsByClassName("speed")[0].textContent;
-temp = parseFloat(temp);
-speed = parseFloat(speed);
+// var temp = document.getElementsByClassName("temp")[0].textContent;
+// var speed = document.getElementsByClassName("speed")[0].textContent;
+// temp = parseFloat(temp);
+// speed = parseFloat(speed);
 
 
-if (temp <= 50 && speed >= 3.0) {
-    var f = 35.74 + 0.6215 * temp - 35.75 * (speed ** 0.16) + 0.4275 * temp * (speed ** 0.16);
-    f = Math.round(f);
-    var chill = f.toString();
-    document.getElementsByClassName("chill")[0].textContent = chill+"*F";
-}
+// if (temp <= 50 && speed >= 3.0) {
+//     var f = 35.74 + 0.6215 * temp - 35.75 * (speed ** 0.16) + 0.4275 * temp * (speed ** 0.16);
+//     f = Math.round(f);
+//     var chill = f.toString();
+//     document.getElementsByClassName("chill")[0].textContent = chill+"*F";
+// }
 
-else {
-    document.getElementsByClassName("chill")[0].textContent = "N/A";
-}
+// else {
+//     document.getElementsByClassName("chill")[0].textContent = "N/A";
+// }

91df1d1fa7660132830c0eac1b18f6bd75031c97
Username-Red <ashkestrel@gmail.com>
2023-07-01 18:05:13 +0200

    adding the s

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -1,7 +1,7 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = "http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=4f3b6ca284c383b4bbd8fbd38d58c9a0";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=4f3b6ca284c383b4bbd8fbd38d58c9a0";
 
 async function apiFetch() {
     try {
